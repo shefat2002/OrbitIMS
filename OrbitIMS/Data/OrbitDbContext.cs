96b99753463de882c7da6eaa1b88a04929b87967
shefat2002 <shefat2002@gmail.com>
2025-10-02 11:42:02 +0600

    Initial Migration Ran

--- OrbitIMS/Data/OrbitDbContext.cs
+++ OrbitIMS/Data/OrbitDbContext.cs
@@ -1,7 +1,20 @@
-﻿namespace OrbitIMS.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrbitIMS.Data
 {
     public class OrbitDbContext : IdentityDbContext
     {
+        public OrbitDbContext(DbContextOptions<OrbitDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
 
     }
 }
--- OrbitIMS/Program.cs
+++ OrbitIMS/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OrbitIMS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DB Connection String
+builder.Services.AddDbContext<OrbitDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+
+////Identity
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<OrbitDbContext>();
+
 
 var app = builder.Build();
 
@@ -9,7 +22,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,6 +30,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
